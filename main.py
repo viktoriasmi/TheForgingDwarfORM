0b550071df8e1210662d1ee9f8993900f2578775
Victoria <viktoria.smi@mail.ru>
2024-04-06 18:45:34 +0300

    создан файл models.py для моделей, сделаны все модели

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print('hello')
+from models import *
---
+++ models.py
@@ -0,0 +1,102 @@
+import datetime
+from peewee import *
+
+db = SqliteDatabase('TheForgingDwarfORM.db')
+
+STATUS_CHOICES = [
+    ('ip', 'In Progress'),
+    ('d', 'Done'),
+    ('c', 'Canceled'),
+]
+
+
+def create_tables_if_not_exist():
+    # Проверяем, существуют ли таблицы, и создаем их, если они не существуют
+    if not db.is_closed():
+        db.connect()
+
+    tables_to_create = [User, Client, CatalogItem, OrderCatalog, OrderIndividual, IncomeData, IncomeIndividualData,
+                        QueueData, QueueIndividualData]
+    for model in tables_to_create:
+        if not model.table_exists():
+            model.create_table(True)  # True для принудительного создания таблицы, даже если она уже существует
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class User(BaseModel):
+    class Meta:
+        table_name = 'users'
+    name = CharField(max_length=100)
+    password = BlobField()
+
+class Client(BaseModel):
+    class Meta:
+        table_name = 'clients'
+    first_name = CharField(max_length=255)
+    last_name = CharField(max_length=255)
+    address = CharField(max_length=255)
+
+class CatalogItem(BaseModel):
+    class Meta:
+        table_name = 'catalog'
+    name = CharField(max_length=255)
+    type = CharField(max_length=255)
+    material = CharField(max_length=255)
+    style = CharField(max_length=255)
+    description = TextField()
+    production_time = IntegerField()
+    price = FloatField()
+
+class OrderCatalog(BaseModel):
+    class Meta:
+        table_name = 'orders_catalog'
+    client = ForeignKeyField(Client)
+    catalog = ForeignKeyField(CatalogItem)
+    created_at = DateTimeField(default=datetime.datetime.now)
+    amount = IntegerField(default=1)
+    status = CharField(choices=STATUS_CHOICES)
+
+class OrderIndividual(BaseModel):
+    class Meta:
+        table_name = 'orders_individual'
+    client = ForeignKeyField(Client)
+    req = TextField(null=False)
+    catalog = ForeignKeyField(CatalogItem)
+    created_at = DateTimeField(default=datetime.datetime.now)
+    amount = IntegerField(default=1)
+    status = CharField(choices=STATUS_CHOICES)
+    price = FloatField()
+
+class IncomeData(BaseModel):
+    class Meta:
+        table_name = 'income'
+    order = ForeignKeyField(OrderCatalog)
+    price = FloatField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+class IncomeIndividualData(BaseModel):
+    class Meta:
+        table_name = 'income_individual'
+    order = ForeignKeyField(OrderIndividual)
+    price = FloatField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+class QueueData(BaseModel):
+    class Meta:
+        table_name = 'queue_regular'
+    order = ForeignKeyField(OrderCatalog)
+    created_at = DateTimeField()
+    status = CharField(choices=STATUS_CHOICES)
+    production_time = IntegerField()
+
+class QueueIndividualData(BaseModel):
+    class Meta:
+        table_name = 'queue_individual'
+    order = ForeignKeyField(OrderIndividual)
+    created_at = DateTimeField()
+    status = CharField(choices=STATUS_CHOICES)
+    production_time = IntegerField()
+
+if __name__ == '__main__':
+    create_tables_if_not_exist()
